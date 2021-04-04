c79c93045cefd1c56494936f86124f87c6c6541f
NagarjunPrem <nagarjunprem@gmail.com>
2021-04-04 19:39:01 -0300

    version 5.0

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const methodOverride = require('method-override')
 const GridFS = require('gridfs');
 var fs = require('fs');
 var formidable = require('formidable');
+const mongodb = require('mongodb');
+const url = 'mongodb://localhost:27017/';
 
 const Schema = mongoose.Schema;
 
@@ -50,7 +52,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
         });
     })
 
- const a = '606a1ae4ce83944a5817a85e'
+ const a = '606a30a6f0edab37e076aa42'
 
 app.get('/mongo-image', function (req, res, next) {
     Image.findById(a, function (err, doc) {
@@ -112,6 +114,57 @@ app.delete('/products/:id', async (req, res) => {
     const deletedProduct = await Product.findByIdAndDelete(id);
     res.redirect('/products');
 })
+
+  
+  
+app.get("/mongo-video", function (req, res) {
+    mongodb.MongoClient.connect(url, { useUnifiedTopology: true }, function (error, client) {
+      if (error) {
+        res.status(500).json(error);
+        return;
+      }
+  
+      const range = req.headers.range;
+      if (!range) {
+        res.status(400).send("Requires Range header");
+      }
+  
+      const db = client.db('farmStand');
+      // GridFS Collection
+      db.collection('fs.files').findOne({}, (err, video) => {
+        if (!video) {
+          res.status(404).send("No video uploaded!");
+          return;
+        }
+  
+        // Create response headers
+        const videoSize = video.length;
+        const start = Number(range.replace(/\D/g, ""));
+        const end = videoSize - 1;
+  
+        const contentLength = end - start + 1;
+        const headers = {
+          "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+          "Accept-Ranges": "bytes",
+          "Content-Length": contentLength,
+          "Content-Type": "video/mp4",
+        };
+  
+        // HTTP Status 206 for Partial Content
+        res.writeHead(206, headers);
+  
+        const bucket = new mongodb.GridFSBucket(db);
+        //Check out your own file name stored in db
+        const downloadStream = bucket.openDownloadStreamByName('MrBean.mp4', {
+          start
+        });
+  
+        // Finally pipe video to response
+        downloadStream.pipe(res);
+      });
+    });
+  });
+  
 
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!!")
